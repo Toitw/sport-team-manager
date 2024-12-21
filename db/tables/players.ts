85c16fb23da7f75d0582c421bd35f6c74f2c8c34
Toitw <juanjrgast@gmail.com>
2024-12-21 20:51:29 +0000

    Checkpoint: Before adding games and minutes played statistics

--- db/tables/players.ts
+++ db/tables/players.ts
@@ -4,10 +4,13 @@ import { positionEnum } from "../types";
 
 export const players = pgTable("players", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
-  teamId: integer("team_id").notNull().references(() => teams.id),
+  teamId: integer("team_id")
+    .notNull()
+    .references(() => teams.id),
   name: text("name").notNull(),
   position: positionEnum("position").notNull(),
   number: integer("number").notNull(),
   photoUrl: text("photo_url"),
-  createdAt: timestamp("created_at", { mode: 'string' }).defaultNow()
+  createdAt: timestamp("created_at", { mode: "string" }).defaultNow(),
 });
+//
